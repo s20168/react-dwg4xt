70a94de0035ec26f4b6045802b8d0f446fc8c33e
s20168 <56789654+s20168@users.noreply.github.com>
2023-06-25 15:48:35 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const submitInfo = (event) => {
+    event.preventDefault();
+    console.log(
+      `Imię: ${firstName}, Nazwisko: ${lastName}, Adres e-mail: ${email}`
+    );
+  };
+
+  return (
+    <Form onSubmit={submitInfo}>
+      <Form.Group controlId="firstName">
+        <Form.Label>Imię: </Form.Label>
+        <Form.Control
+          type="text"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="lastName">
+        <Form.Label>Nazwisko: </Form.Label>
+        <Form.Control
+          type="text"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="email">
+        <Form.Label>Adres e-mail: </Form.Label>
+        <Form.Control
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Zarejestruj się
+      </Button>
+    </Form>
+  );
+}
+
+App.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+export default App;
